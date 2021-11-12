e4d3d1b21cb39ba183412cff035a78d91066bf9e
Giuseppe Luzzi <me@giuseppeluzzi.com>
2021-11-12 12:33:08 +0100

    feat: login screen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -4,64 +4,84 @@ import * as React from "react";
 import {
   StyleSheet,
   View,
-  Text,
   Touchable,
-  TouchableOpacity
+  TouchableOpacity,
+  Button,
+  TextInput
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Svg, { Path } from "react-native-svg";
+import tailwind from "tailwind-rn";
+import { Text } from "../components/Themed";
 import { RootStackParamList, RootStackScreenProps } from "../types";
 
 export default function WelcomeScreen({
   navigation
 }: NativeStackScreenProps<RootStackParamList, "Welcome">) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+    <SafeAreaView style={tailwind("bg-gray-100 pt-6")}>
+      <View style={tailwind("px-8 ")}>
+        <View style={tailwind("items-end")}>
+          <TouchableOpacity>
+            <Text style={tailwind("font-medium")}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={tailwind("pt-16 font-medium text-4xl")}>Login</Text>
+        <Text style={tailwind("pt-2 font-light")}>Welcome back!</Text>
+
+        <View style={tailwind("pt-10")}>
+          <Text style={tailwind("uppercase font-medium text-sm")}>Email</Text>
+          <TextInput
+            style={tailwind(
+              "mt-2 py-4 px-6 font-medium text-black bg-white rounded-lg"
+            )}
+            value="peppe.luzzi@gmail.com"
+          />
+        </View>
+        <View style={tailwind("pt-6")}>
+          <Text style={tailwind("uppercase font-medium text-sm")}>
+            Password
+          </Text>
+          <TextInput
+            secureTextEntry={true}
+            style={tailwind(
+              "mt-2 py-4 px-6 font-medium text-black bg-white rounded-lg"
+            )}
+            value="password"
+          />
+        </View>
+      </View>
       <TouchableOpacity
-        style={{ marginTop: 30 }}
-        onPress={async () => {
+        style={tailwind("self-end w-3/4 pt-10")}
+        /*onPress={async () => {
           await signInWithEmailAndPassword(
             getAuth(),
             "peppe.luzzi@gmail.com",
             "ciaociao"
           );
-        }}
+        }}*/
       >
         <View
-          style={{
-            backgroundColor: "red",
-            paddingVertical: 15,
-            paddingHorizontal: 25,
-            borderRadius: 1000
-          }}
+          style={tailwind(
+            "flex-row items-center justify-between px-6 py-4 bg-yellow-400 rounded-tl-lg rounded-bl-lg"
+          )}
         >
-          <Text
-            style={{
-              color: "white",
-              textTransform: "uppercase",
-              fontWeight: "bold"
-            }}
+          <Text style={tailwind("font-medium text-lg")}>Log In</Text>
+          <Svg
+            style={tailwind("h-6 w-6 text-black")}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            Login
-          </Text>
+            <Path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </Svg>
         </View>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "bold"
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%"
-  }
-});
