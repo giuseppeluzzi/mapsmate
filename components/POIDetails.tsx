1113836f7d10a51d962e53a9d8028f3ed44bd6cd
Giuseppe Luzzi <me@giuseppeluzzi.com>
2022-07-15 00:29:54 +0200

    feat: review options button

--- components/POIDetails.tsx
+++ components/POIDetails.tsx
@@ -505,7 +505,7 @@ export const POIDetails = ({
                     position={"absolute"}
                     right={"0"}
                   >
-                    <Icon fill="#black" viewBox="0 0 20 20">
+                    <Icon fill="black" viewBox="0 0 20 20">
                       <Path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
                     </Icon>
                   </Button>
