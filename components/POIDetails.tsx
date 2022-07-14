67e438e6c0a9d21c87471b80adb4cdeafead38b4
Giuseppe Luzzi <me@giuseppeluzzi.com>
2022-07-15 00:23:44 +0200

    feat: improve poi details data

--- components/POIDetails.tsx
+++ components/POIDetails.tsx
@@ -352,20 +352,38 @@ export const POIDetails = ({
               </TouchableOpacity>
             )}
             {!!poi.phone && poi.phone.length > 0 && (
-              <HStack space={3} py={3} px={3} alignItems={"center"}>
-                <Icon fill="gray" viewBox="0 0 20 20" width={20} height={20}>
-                  <Path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                </Icon>
-                <Text>{poi.phone}</Text>
-              </HStack>
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL(
+                    "tel:" + poi.phone.replace("+", "00").replace(/\D/g, "")
+                  );
+                }}
+              >
+                <HStack space={3} py={3} px={3} alignItems={"center"}>
+                  <Icon fill="gray" viewBox="0 0 20 20" width={20} height={20}>
+                    <Path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  </Icon>
+                  <Text>{poi.phone}</Text>
+                </HStack>
+              </TouchableOpacity>
             )}
             {!!poi.website && poi.website.length > 0 && (
-              <HStack space={3} py={3} px={3} alignItems={"center"}>
-                <Icon fill="gray" viewBox="0 0 20 20" width={20} height={20}>
-                  <Path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-                </Icon>
-                <Text>{poi.website ?? ""}</Text>
-              </HStack>
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL(poi.website);
+                }}
+              >
+                <HStack space={3} py={3} px={3} alignItems={"center"}>
+                  <Icon fill="gray" viewBox="0 0 20 20" width={20} height={20}>
+                    <Path
+                      fillRule="evenodd"
+                      d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z"
+                      clipRule="evenodd"
+                    />
+                  </Icon>
+                  <Text>{poi.website ?? ""}</Text>
+                </HStack>
+              </TouchableOpacity>
             )}
             {poi.workhours && (
               <TouchableOpacity
